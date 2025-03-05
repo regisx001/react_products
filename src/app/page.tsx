f24e0c848907cca3a3bbe5b04c5920898c716608
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-03-05 16:19:27 +0000

    FEATURE: Fetching products and categories from the server.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
 
-export default function Home() {
+async function Home() {
+  const products = await (await fetch("http://localhost:8000/api/products/")).json()
+  const categories = await (await fetch("http://localhost:8000/api/categories/")).json()
+
   return (
     <div className="">
-      Hello World
+
+      <h1>
+        Products
+      </h1>
+      <pre className="pre">
+        {JSON.stringify(products, null, 2)}
+      </pre>
+
+      <h1 >
+        Categories
+      </h1>
+      <pre className="pre">
+        {JSON.stringify(categories, null, 2)}
+      </pre>
+
     </div>
   );
 }
+
+
+
+export default Home;
